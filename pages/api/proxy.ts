1307a768c292eb768aba16c748cc8a12baf38ca0
Zachary Lai <ilzy@foxmail.com>
2023-04-23 20:13:45 +0800

    Update proxy.ts
    only usa

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -4,23 +4,10 @@ export const config = {
   runtime: "edge", // this is a pre-requisite
   // exclude hongkong cause it's not supported by OpenAI
   regions: [
-    "sin1",
-    "cdg1",
-    "arn1",
-    "dub1",
-    "lhr1",
-    "iad1",
-    "sfo1",
-    "pdx1",
     "cle1",
-    "gru1",
-    "hnd1",
-    "icn1",
-    "kix1",
-    "bom1",
-    "syd1",
-    "fra1",
-    "cpt1",
+    "iad1",
+    "pdx1",
+    "sfo1",
   ],
 };
 
